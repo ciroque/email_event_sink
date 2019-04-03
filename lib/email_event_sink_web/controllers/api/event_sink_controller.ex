d7cdca96fb0f033d50aa1238438e5df175307923
Steve Wagner <ciroque@live.com>
2019-04-03 07:16:26 -0700

    Put the timestamp in the right place

--- lib/email_event_sink_web/controllers/api/event_sink_controller.ex
+++ lib/email_event_sink_web/controllers/api/event_sink_controller.ex
@@ -1,8 +1,8 @@
 defmodule EmailEventSinkWeb.EventSinkController do
   use EmailEventSinkWeb, :controller
 
-  def create(conn, %{"email" => _email, "event" => _event, "timestamp" => _event_timestamp} = event) do
-    {:ok, created} = Events.create(event)
+  def create(conn, %{"email" => _email, "event" => _event, "timestamp" => event_timestamp} = event) do
+    {:ok, created} = Events.create(event |> Map.put_new("event_timestamp", event_timestamp))
     conn |> render("create.json", %{event: created})
   end
 end
