87c1b3c0affaee3f2c7675b579f6111dd665d86e
swagner <swagner@marchex.com>
2019-04-22 08:59:23 -0700

    Adds matcher for actual SendGrid POST

--- lib/email_event_sink_web/controllers/api/event_sink_controller.ex
+++ lib/email_event_sink_web/controllers/api/event_sink_controller.ex
@@ -15,6 +15,19 @@ defmodule EmailEventSinkWeb.EventSinkController do
         end)
     conn |> render("create.json", %{event: %{}})
   end
+  
+  def create(conn, events) do
+    Logger.info("#{__MODULE__}::create #{inspect(events)}")
+    events |>
+      Enum.map(
+        fn %{"timestamp" => event_timestamp, "smtp-id" => smtp_id} = event ->
+          attrs = event
+            |> Map.put_new("event_timestamp", event_timestamp)
+            |> Map.put_new("smtp_id", smtp_id)
+          Events.create(attrs)
+        end)
+    conn |> render("create.json", %{event: %{}})
+  end
 
   def index(conn, params) do
     Logger.info("#{__MODULE__}::get #{inspect(params)}")
