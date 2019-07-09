0df9a7424284cf588105ac4ba9f523f0d870dff6
Steve Wagner <ciroque@live.com>
2019-07-09 09:23:49 -0700

    Adds logging for event row creation

--- lib/email_event_sink/events.ex
+++ lib/email_event_sink/events.ex
@@ -3,10 +3,16 @@ defmodule Events do
 
   alias EmailEventSink.Repo
 
+  require Logger
+
   def create(attrs \\ %{}) do
-    %Event{}
+    result = %Event{}
     |> Event.changeset(attrs)
     |> Repo.insert()
+    
+    Logger.info("Created new event: #{inspect(result)}")
+    
+    result
   end
 
   def get(), do: Event |> Repo.all
